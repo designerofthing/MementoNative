ed2b7999a21cd4a37677e7bed3546d0e718dcf3b
designerofthing <designerofthings@gmail.com>
2020-12-10 10:27:35 +0100

    adds navigation and authentication to App.jsx

--- App.js
+++ App.js
@@ -1,30 +1,93 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, ActivityIndicator } from "react-native";
 import { AppLoading } from "expo";
 import { useFonts, Taviraj_400Regular } from "@expo-google-fonts/dev";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Amplify, { Auth } from "aws-amplify";
+import SignIn from './src/screens/SignIn';
+import SignUp from './src/screens/SignUp';
+import ConfirmSignUp from './src/screens/ConfirmSignUp';
+import Home from './src/screens/Home';
 import config from "./aws-exports";
-import { withAuthenticator } from "aws-amplify-react-native";
 Amplify.configure(config);
 
 function App() {
-  async function signOut() {
-    try {
-      await Auth.signOut();
-    } catch (error) {
-      console.log("Error signing out: ", error);
-    }
-  }
+  const [isUserLoggedIn, setUserLoggedIn] = useState('initializing');
+
+  
+  const AuthenticationStack = createStackNavigator();
+  const AppStack = createStackNavigator();
+  const AuthenticationNavigator = props => {
+    return (
+      <AuthenticationStack.Navigator headerMode="none">
+        <AuthenticationStack.Screen name="SignIn">
+          {screenProps => (
+            <SignIn {...screenProps} updateAuthState={props.updateAuthState} />
+            )}
+        </AuthenticationStack.Screen>
+        <AuthenticationStack.Screen name="SignUp" component={SignUp} />
+        <AuthenticationStack.Screen
+          name="ConfirmSignUp"
+          component={ConfirmSignUp}
+          />
+      </AuthenticationStack.Navigator>
+    );
+  };
+  const AppNavigator = props => {
+    return (
+      <AppStack.Navigator>
+        <AppStack.Screen name="Home">
+          {screenProps => (
+            <Home {...screenProps} updateAuthState={props.updateAuthState} />
+            )}
+        </AppStack.Screen>
+      </AppStack.Navigator>
+    );
+  };
   let [fontsLoaded] = useFonts({
     Taviraj_400Regular,
   });
+  const Initializing = () => {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" color="tomato" />
+        </View>
+      );
+    };
 
+    useEffect(() => {
+          checkAuthState();
+        }, []);
+        async function checkAuthState() {
+          try {
+            await Auth.currentAuthenticatedUser();
+            console.log(' User is signed in');
+            setUserLoggedIn('loggedIn');
+          } catch (err) {
+            console.log(' User is not signed in');
+            setUserLoggedIn('loggedOut');
+          }
+        }
+        function updateAuthState(isUserLoggedIn) {
+          setUserLoggedIn(isUserLoggedIn);
+        }
+  
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
       <View style={styles.container}>
+        <NavigationContainer>
+        {isUserLoggedIn === 'initializing' && <Initializing />}
+        {isUserLoggedIn === 'loggedIn' && (
+          <AppNavigator updateAuthState={updateAuthState} />
+        )}
+        {isUserLoggedIn === 'loggedOut' && (
+          <AuthenticationNavigator updateAuthState={updateAuthState} />
+        )}
+      </NavigationContainer>
         <Button title="Sign Out" color="silver" onPress={signOut} />
         <Text style={styles.logo}>Memento </Text>
         <Text style={styles.text}>Memorial App</Text>
@@ -33,7 +96,9 @@ function App() {
     );
   }
 }
-export default withAuthenticator(App);
+export default App;
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
